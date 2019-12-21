fdc4db83d5926df43921da6f4395f26d5d2a00d1
Shiro <sherifmohamed2004@gmail.com>
2019-12-21 14:58:17 +0200

    lottie and icons

--- app/src/main/java/com/sherif/nearbyapp/ui/MainActivity.kt
+++ app/src/main/java/com/sherif/nearbyapp/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
 import android.widget.TextView
 import android.widget.Toast
@@ -38,9 +39,17 @@ class MainActivity : AppCompatActivity() {
         chooseMode()
 
         initMode()
+        val mainHandler = Handler(Looper.getMainLooper())
+        mainHandler.post(object : Runnable {
+            override fun run() {
+             val Distance =  measure(37.4406,-122.1430,37.4429 ,-122.1444)
+
+                val toast = Toast.makeText(applicationContext, Distance.toString(), Toast.LENGTH_SHORT)
+                toast.show()
+                mainHandler.postDelayed(this, 10000)
+            }
+        })
     }
-
-
 
     @SuppressLint("MissingPermission")
     private fun getLastLocation() {
@@ -52,6 +61,7 @@ class MainActivity : AppCompatActivity() {
                     if (location == null) {
                         requestNewLocationData()
                     } else {
+
                         findViewById<TextView>(R.id.latTextView).text = location.latitude.toString()
                         findViewById<TextView>(R.id.lonTextView).text = location.longitude.toString()
                     }
@@ -86,14 +96,9 @@ class MainActivity : AppCompatActivity() {
             var mLastLocation: Location = locationResult.lastLocation
             findViewById<TextView>(R.id.latTextView).text = mLastLocation.latitude.toString()
             findViewById<TextView>(R.id.lonTextView).text = mLastLocation.longitude.toString()
-        }
-    }
+            val toast = Toast.makeText(applicationContext, mLastLocation.latitude.toString(), Toast.LENGTH_SHORT)
+            toast.show()
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == LOCATION_REQUEST_CODE_PERMISSION) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                getLastLocation()
-            }
         }
     }
 
@@ -108,11 +113,20 @@ class MainActivity : AppCompatActivity() {
           } }
   }
 
-
     fun initMode(){
         textchossenmode.text = sharedPref.getMode()
     }
-
+    fun measure(latitude1:Double, longitude1:Double, latitude2:Double, longitude2:Double):Double{  // generally used geo measurement function
+        var radius = 6378.137; // Radius of earth in KM
+        var dlatitude = latitude2 * Math.PI / 180 - latitude1 * Math.PI / 180;
+        var dlongitude = longitude2 * Math.PI / 180 - longitude1 * Math.PI / 180;
+        var area = Math.sin(dlatitude/2) * Math.sin(dlatitude/2) +
+                Math.cos(latitude1 * Math.PI / 180) * Math.cos(latitude2 * Math.PI / 180) *
+                Math.sin(dlongitude/2) * Math.sin(dlongitude/2);
+        var circumference = 2 * Math.atan2(Math.sqrt(area), Math.sqrt(1-area));
+        var ditance = radius * circumference;
+        return ditance * 1000; // meters
+    }
 }
 
 
--- app/src/main/java/com/sherif/nearbyapp/ui/SplashActivity.kt
+++ app/src/main/java/com/sherif/nearbyapp/ui/SplashActivity.kt
@@ -3,15 +3,12 @@ package com.sherif.nearbyapp.ui
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.sherif.nearbyapp.R
 import com.sherif.nearbyapp.utils.LOCATION_REQUEST_CODE_PERMISSION
 import com.sherif.nearbyapp.utils.LocationUtils
-
-import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.*
 
 class SplashActivity : AppCompatActivity() {
     lateinit var  locationUtils: LocationUtils
@@ -27,31 +24,39 @@ class SplashActivity : AppCompatActivity() {
         if (requestCode == LOCATION_REQUEST_CODE_PERMISSION) {
             if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                 if (locationUtils.checkPermissions()) {
-                    intent = Intent(applicationContext, MainActivity::class.java)
-                    startActivity(intent)
+
+                    Timer().schedule(object : TimerTask() {
+                        override fun run() {
+                            intent = Intent(applicationContext, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }, 2500)
                 }
 
             }
             else{
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("Location Permission")
-                builder.setMessage("Please accept the location permission the application wont work without it")
-
-                // Set a positive button and its click listener on alert dialog
-                builder.setPositiveButton("YES"){dialog, which ->
-                    locationUtils.requestPermissions(this)
-                }
-
-
-                // Display a negative button on alert dialog
-                builder.setNegativeButton("No"){dialog,which ->
-                    finish()
-                }
-                val dialog: AlertDialog = builder.create()
-                dialog.show()
-
+            permissionDialog()
             }
         }
+    }
+
+
+
+    fun permissionDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Location Permission")
+        builder.setMessage("Please accept the location permission the application wont work without it")
+
+        // Set a positive button and its click listener on alert dialog
+        builder.setPositiveButton("YES"){dialog, which ->
+            locationUtils.requestPermissions(this)
+        }
+        // Display a negative button on alert dialog
+        builder.setNegativeButton("No"){dialog,which ->
+            finish()
+        }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 
 }
